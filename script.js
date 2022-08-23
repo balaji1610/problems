7032c6f3cad585cf4b9211d4cdbaa765439c9b24
balaji1610 <balajiece1610>
2022-08-24 04:31:29 +0530

    Commit

---
+++ script.js
@@ -0,0 +1,67 @@
+const Sum = {
+  X: 10,
+  Y: 85,
+  D: 30,
+
+  //y-x=D // Steps
+
+  //y=D*3+x // Y value
+
+  //
+
+  FinalDestination: function () {
+    return this.D * 3 + this.X;
+  },
+};
+
+// console.log(`${Sum.FinalDestination()}`);
+
+const FindX = {
+  Y: 85,
+  D: 30,
+
+  FindX: function () {
+    return this.D * 3 - this.Y;
+  },
+};
+
+console.log(`${FindX.FindX()}," <---X,   Y: 85,D: 30,"`);
+
+const FindY = {
+  X: 10,
+  D: 30,
+
+  FindY: function () {
+    return this.D * 3 + this.X;
+  },
+};
+
+console.log(`${FindY.FindY()},",<--Y , X: 10,D: 30,"`);
+
+const MinimalJumph = {
+  X: 10,
+  Y: 85,
+
+  MinimalLength: function () {
+    var Jumph = this.Y - this.X;
+    return Jumph / 3;
+  },
+};
+console.log(`${MinimalJumph.MinimalLength()},"<---D  X: 10,Y: 85,`);
+function ListofJumps(X, D) {
+  const First = X + D;
+
+  const Second = First + D;
+
+  const Third = Second + D;
+
+  const overallSteps = [X, First, Second, Third];
+
+  return console.log(overallSteps, "ListOfJumps");
+}
+// ListofJumps(Sum.X, Sum.D);
+ListofJumps(FindX.FindX(), FindX.D); //Find x
+
+ListofJumps(FindY.X, FindY.D); // Find Y
+
+ListofJumps(MinimalJumph.X, MinimalJumph.MinimalLength());
