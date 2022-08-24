b61052e5fdfb17f95216d43ebb901a7fb820101f
balaji1610 <balajiece1610>
2022-08-25 03:37:40 +0530

    Find_distance

--- script.js
+++ script.js
@@ -88,3 +88,78 @@ ListofJumps(FindX.FindX(), FindX.D); //Find x
 ListofJumps(FindY.X, FindY.D); // Find Y
 
 ListofJumps(MinimalJumph.X, MinimalJumph.MinimalLength());
+
+//functions
+
+//Find Minimal jumph
+
+const Numberofjumphs = function () {
+  const x = Number(document.querySelector(".x").value);
+
+  const y = Number(document.querySelector(".y").value);
+
+  const D = Number(document.querySelector(".D").value);
+  if (isNaN(x) || x < 0 || x == 0) {
+    alert("Not Validate");
+    return false;
+  } else if (isNaN(y) || y < 0 || y == 0) {
+    alert("Not Validate");
+    return false;
+  } else if (isNaN(D) || D < 0 || D == 0) {
+    alert("Not Validate");
+    return false;
+  }
+
+  var Jumps = (y - x) / D;
+
+  var FinalJumps = Math.round(Math.abs(Jumps));
+
+  document.querySelector(
+    ".jumpsText"
+  ).textContent = `The Minimal Jumps ${FinalJumps}`;
+  console.log(FinalJumps, "JUMPS");
+
+  const First = x + D;
+
+  const Second = First + D;
+
+  const Third = Second + D;
+
+  const overallSteps = [x, First, Second, Third];
+
+  console.log(overallSteps, "overallsteps");
+};
+
+const FindDistance = function () {
+  const x1 = Number(document.querySelector(".x1").value);
+
+  const y1 = Number(document.querySelector(".y1").value);
+
+  if (isNaN(x1) || x1 < 0 || x1 == 0) {
+    alert("Not Validate");
+    return false;
+  } else if (isNaN(y1) || y1 < 0 || y1 == 0) {
+    alert("Not Validate");
+    return false;
+  }
+
+  var Distance = (y1 - x1) / 3;
+  console.log(Distance, "--DD");
+
+  var FinalDistance = Math.round(Math.abs(Distance));
+
+  document.querySelector(
+    ".Distance"
+  ).textContent = `The Distance ${FinalDistance}`;
+  console.log(FinalDistance, "Find Distance");
+};
+
+document.querySelector(".submitbtn").addEventListener("click", function () {
+  Numberofjumphs();
+});
+
+document
+  .querySelector(".submitFindDistance")
+  .addEventListener("click", function () {
+    FindDistance();
+  });
